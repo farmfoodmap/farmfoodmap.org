3e159915723f80e738864e05b4771471360af0fd
Super Phat Arrow <38541071+SuperPhatArrow@users.noreply.github.com>
2022-12-26 19:57:23 +0000

    social links and new maps

--- src/main.ts
+++ src/main.ts
@@ -30,15 +30,79 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
    <img src="/android-chrome-384x384.png"/>
    <span>FOOD FARM MAP</span>
 </div>
-<div id="infobar">Info</div>
+<div id="infobar"><a href="https://twitter.com/farmfoodmap" target="_blank" noreferrer="true"><svg xmlns="http://www.w3.org/2000/svg" class="svg-social" id="svg-icon-twitter" viewBox="0 0 512 512">
+<path d="M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z"
+  />
+</svg></a>&nbsp;&nbsp;&nbsp;<a href="https://github.com/farmfoodmap" target="_blank" noreferrer="true"><svg xmlns="http://www.w3.org/2000/svg" class="svg-social" id="svg-icon-github" viewBox="0 0 512 512">
+<path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"/>
+</svg></a><br><span><span></div>
 <div id="map"></div>
 `;
-const map = L.map('map').setView([0, 0], 3);
+const mbAttr =
+  'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const mbUrl =
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+});
+const streets = L.tileLayer(mbUrl, {
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  attribution: mbAttr,
+});
+const satellite = L.tileLayer(mbUrl, {
+  id: 'mapbox/satellite-v9',
+  tileSize: 512,
+  zoomOffset: -1,
+  attribution: mbAttr,
+});
+const darkMatter = L.tileLayer(
+  'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20,
+  }
+);
+const cycle = L.tileLayer(
+  'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+  {
+    maxZoom: 20,
+    attribution:
+      '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
+const railway = L.tileLayer(
+  'https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
+  {
+    maxZoom: 19,
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map style: &copy; <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+  }
+);
+const baseLayers = {
+  OpenStreetMap: osm,
+  Streets: streets,
+  Satellite: satellite,
+  Dark: darkMatter,
+  Cycle: cycle,
+  Railway: railway,
+};
+
+const map = L.map('map', {
+  layers: [osm],
+}).setView([0, 0], 3);
 const moveMapToSavedPosition = () => {
   const c = JSON.parse(localStorage.center);
   const z = parseInt(localStorage.zoom);
   map.setView(c, z);
 };
+L.control.layers(baseLayers).addTo(map);
+
 try {
   const s = location.search;
   if (s && s.includes('lat=') && s.includes('lng=') && s.includes('z=')) {
@@ -64,16 +128,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.addLayer(markers);
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
-
-document.querySelector('.leaflet-control-attribution')!.innerHTML =
-  document.querySelector('.leaflet-control-attribution')?.innerHTML
-    ? '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer" class="!text-link hover:!text-hover !no-underline transition-colors">OpenStreetMap</a> contributors'
-    : '';
+const attributionBox = document.querySelector(
+  '.leaflet-bottom.leaflet-right>.leaflet-control-attribution'
+);
+attributionBox!.innerHTML = attributionBox?.innerHTML
+  ? '&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer" class="!text-link hover:!text-hover !no-underline transition-colors">OpenStreetMap</a> contributors'
+  : '';
 
 const updateInfo = (message = 'MAP IS READY') => {
-  const infobar = document.getElementById('infobar');
-  if (infobar) {
-    document.getElementById('infobar')!.innerText = message;
+  const infoBar = document.querySelector('#infobar>span');
+  if (infoBar) {
+    infoBar!.innerHTML = message;
   }
 };
 updateInfo();
